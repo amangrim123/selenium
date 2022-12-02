5fe4707a0753bac11470f8493a6f6e90b4aa5d56
unknown <aman.grimebyte@gmail.com>
2022-12-02 15:05:55 +0530

    ok

--- sel_asy_copy_4.py
+++ sel_asy_copy_4.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
     chrome_options.add_argument("--proxy-server='direct://'")
     chrome_options.add_argument("--proxy-bypass-list=*")
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')

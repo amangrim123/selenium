00fac6d822757e12b70061e4696afe56107f66b9
unknown <aman.grimebyte@gmail.com>
2022-12-05 16:04:22 +0530

    ok

--- two_driver.py
+++ two_driver.py
@@ -264,12 +264,18 @@ if __name__ == "__main__":
 
     p1 = multiprocessing.Process(target=all_process,args=(containt_list,))
     p2 = multiprocessing.Process(target=all_process,args=(containt_list,))
+    p3 = multiprocessing.Process(target=all_process,args=(containt_list,))
+    p4 = multiprocessing.Process(target=all_process,args=(containt_list,))
 
     p1.start()
     p2.start()
+    p3.start()
+    p4.start()
 
     p1.join()
     p2.join()
+    p3.join()
+    p4.join()
 
     # executor = ThreadPoolExecutor(10)
     # def scrape(url, *, loop):

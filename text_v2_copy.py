8e1b1279abf398287072c80d157109c332a1809d
unknown <aman.grimebyte@gmail.com>
2022-12-05 14:24:28 +0530

    ok

--- text_v2_copy.py
+++ text_v2_copy.py
@@ -81,10 +81,10 @@ chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
 # chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--ignore-certificate-errors')
+# chrome_options.add_argument('--ignore-certificate-errors')
 
 driver_path ="/usr/bin/chromedriver"
 #driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)

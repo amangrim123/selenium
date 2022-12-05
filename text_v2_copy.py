fa062d3b5cc1c87c792defcfb0fe23b074281430
unknown <aman.grimebyte@gmail.com>
2022-12-05 14:34:27 +0530

    ok

--- text_v2_copy.py
+++ text_v2_copy.py
@@ -96,7 +96,7 @@ driver_path ="/usr/bin/chromedriver"
 s = Service(driver_path)
 options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s,options=chrome_options)
 # driver = webdriver.Chrome(options=chrome_options, executable_path = driver_path)
 
 quill_login(driver)

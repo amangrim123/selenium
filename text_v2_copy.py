0fbfac41fe88d1bcaadd76f1001464cb0f2c8bab
unknown <aman.grimebyte@gmail.com>
2022-12-05 12:32:27 +0530

    ok

---
+++ text_v2_copy.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+from selenium import webdriver
+executor = ThreadPoolExecutor(10)
+def scrape(url, *, loop):
+    loop.run_in_executor(executor, scraper, url)
+def scraper(url):
+    options = Options()
+
+    options.headless = True
+
+    driver_path ="/usr/bin/chromedriver"
+
+    s = Service(driver_path)
+
+    driver = webdriver.Chrome(options=options,service=s)
+
+    driver.get(url)
+    print(driver.title)
+    driver.quit()
+
+loop = asyncio.get_event_loop()
+for url in ["https://www.google.com/search?q=godaddy"] * 10:
+    scrape(url, loop=loop)
+loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop)))

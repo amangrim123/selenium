8ec7770a88e8787e15b3d37e52329c238b859851
unknown <aman.grimebyte@gmail.com>
2022-12-21 10:04:54 +0530

    ok

--- quils_v3.py
+++ quils_v3.py
@@ -88,7 +88,7 @@ def all_process(containt,db):
         driver.get("https://quillbot.com/login")
         quill_user = "rajan@grimbyte.com"
         quill_pwd = "Grimbyte123."
-        delay = 3 # seconds
+        delay = 20 # seconds
         try:
             myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[3]/section[1]/div/div/div/div/div/div[3]/div/div[3]/div/div/input')))
             #print("Page is ready!")
@@ -255,7 +255,7 @@ def all_process(containt,db):
                         
                         
                     except IndexError:
-                        mycursor2.execute("update bulk_feed_content set content_modify=%s,status=0 where bfc_id=%s", (str(soup1),first_word))
+                        mycursor1.execute("update bulk_feed_content set content_modify=%s,status=0 where bfc_id=%s", (str(soup1),first_word))
                         db.commit()
                         print("exception")
                         flag=0
